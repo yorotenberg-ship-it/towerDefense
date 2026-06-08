52a2bda151455a3b925a6355361581170ea05ec3
Yonah R <yo.rotenberg@gmail.com>
2026-06-08 14:17:09 -0400

    more cash on start

--- tower defense.py
+++ tower defense.py
@@ -12,7 +12,7 @@ cash = 500
 towersWidth = 50
 towersHeight = 50
 
-print('meowmoew')
+x = 2
 
 class Tower:
     def __init__ (Tower, towerX, towerY, width, height, towerType = None, sellerType = False, sellerCost = 0):
